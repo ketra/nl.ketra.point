fddd666aee8ab39f536d4b70ca20118de948e954
Kevin Traa <ketra90@gmail.com>
2018-09-28 16:31:45 +0200

    Added Modules. Completed Authentication

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 
 const Homey = require('homey');
 const appname = "nl.ketra.point"
-const util = require('./Lib/utils')
+const util = require('./Lib/utils.js')
 
 class PointApp extends Homey.App {
 
-	onInit() {
+    onInit() {
+        var utils = new util();
 		this.log('PointApp is running...');
-		util.logtoall("Main","App Started")
+		utils.logtoall("Main","App Started")
 	}
-	
 }
 
-module.exports = MyApp;
+module.exports = PointApp;
--- drivers/Point/driver.js
+++ drivers/Point/driver.js
@@ -5,31 +5,9 @@ const util = require('../../Lib/utils')
 
 class Point extends Homey.Driver  {
 
-    onPair( socket ) {
-		//console.log(Homey.env.CLIENT_ID)
-		let ruri = ' http://localhost:8000'
-        let apiUrl = 'https://api.minut.com/v1/oauth/authorize?client_id=' + Homey.env.CLIENT_ID + '&response_type=code&redirect_uri=' + ruri
-        util.logtoall("Pair","Started Pairing.")
-        let myOAuth2Callback = new Homey.CloudOAuth2Callback(apiUrl)
-            myOAuth2Callback
-                .on('url', url => {
-
-                    // dend the URL to the front-end to open a popup
-                    socket.emit('url', url);
-
-                })
-                .on('code', code => {
-
-                    // ... swap your code here for an access token
-
-                    // tell the front-end we're done
-                    socket.emit('authorized');
-                })
-                .generate()
-                .catch( err => {
-                    util.logtoall("Pair","Error Received on Pair: " + err)
-                    socket.emit('error', err);
-                })
+    onPair(socket) {
+        var utils = new util()
+        utils.startOath(socket);
 
     }
 
